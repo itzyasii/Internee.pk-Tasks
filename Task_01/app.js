e545cc04f95f844def99e2e7c1388ca46dce5b45
Yasir Ali Pechuho <Yasirpechuho1@gmail.com>
2024-10-28 20:04:11 +0500

    Created rest apis for User

--- Task_01/app.js
+++ Task_01/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes')
+const connectDB = require('./config/db');
+
 
 
 dotenv.config();
-
+connectDB();
 const app = express();
 app.use(bodyParser.json());
-
+app.use('api/users', userRoutes);
 
 module.exports = app;
 
---
+++ Task_01/controllers/userController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User')
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
+// Register New User
+
+exports.register = async(req, res) => {
+    try {
+        const {name, password, email} = req.body;
+        
+    } catch (error) {
+        
+    }
+
+};
+
+exports.login = async(req, res) => {
+
+};
+
+exports.getUser = async(req, res) => {
+
+};
+
+
+exports.updateUser = async(req, res) => {
+
+};
+
+exports.deleteUser = async(req, res) => {
+
+};
