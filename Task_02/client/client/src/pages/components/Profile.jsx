6513698859a0cfcb71665abaf4ca32329a6379cd
Yasir Ali Pechuho <Yasirpechuho1@gmail.com>
2024-11-07 17:03:36 +0500

    Design the Profile area

--- Task_02/client/client/src/pages/components/Profile.jsx
+++ Task_02/client/client/src/pages/components/Profile.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { Avatar, Box, Typography } from "@mui/material";
 function Profile() {
-  return (
-    <div>Profile</div>
-  )
+  return <Box sx={{ height: "100%", background: "#ccc", width: "25vw", display:"flex", alignItems:"center", justifyContent:"center", flexDirection:"column" }}>
+
+    <Avatar src="/static/images/avatar/1.jpg" sx={{width:"156px", height:"156px",}}/>
+    <Typography mt={3} variant="h4" sx={{textTransform:"uppercase", color:"primary.light"}}>
+      Yasir Ali Pechuho
+    </Typography>
+    <Typography variant="subtitle1" >
+      Full Stack Developer
+    </Typography>
+    <Typography variant="body2" >
+      yasirpechuho1@gmail.com
+    </Typography>
+  </Box>;
 }
 
-export default Profile
+export default Profile;
