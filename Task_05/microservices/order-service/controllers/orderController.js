857f4b929cc95543316feb06d80968689271e577
Yasir Ali Pechuho <Yasirpechuho1@gmail.com>
2024-12-09 09:55:06 +0500

    all 3 services are in running state and created yaml for dockerfile

--- Task_05/microservices/order-service/controllers/orderController.js
+++ Task_05/microservices/order-service/controllers/orderController.js
@@ -1,4 +1,4 @@
-const Order = require("../models/Order");
+const Order = require("../models/Orders");
 const axios = require("axios");
 
 exports.createOrder = async (req, res) => {
--- Task_05/microservices/user-service/index.js
+++ Task_05/microservices/user-service/index.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const connectDB = require("./config/dbConnection");
 const userRoutes = require("./routes/userRoutes");
 
+// Load environment variables
 dotenv.config();
+
+// Connect to the database
+connectDB();
+
 const app = express();
 
+// Middleware
 app.use(express.json());
+
+// Routes
 app.use("/api/users", userRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("User Service DB Connected"))
-  .catch((err) => console.error(err));
-
+// Start the server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`User Service running on port ${PORT}`));
